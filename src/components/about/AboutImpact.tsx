bc25bcf83efb10c104f6a948cc01c37ade05e261
Alexotieno1717 <alexotieno900@gmail.com>
2025-06-04 15:56:47 +0300

    [fix]: fix typo error

--- src/components/about/AboutImpact.tsx
+++ src/components/about/AboutImpact.tsx
@@ -11,7 +11,7 @@ const AboutImpact = () => {
                 <p className='text-white'>
                     A movement of faith, fire, and legacy. Join the thousands called to rise. <br/><br/>
 
-                    Rhema Feast is a significant annual spiritual event that began in 2015. This year's Rhema Feast will take place 1st -5th September 2025 <br/>
+                    Rhema Feast is a significant annual spiritual event that began in 2015. This year&#39;s Rhema Feast will take place 1st -5th September 2025 <br/>
                     We are an apostolic movement led by visionary Bishop and entrepreneur Julian Kyula, and its mission is to share the unadulterated and authentic word of God to nations and all generations.
                     <br/><br/>
 
