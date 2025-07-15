9ed34da7e76d36cbab9754bca77fa1339dd56653
kepha-okari <kephaokari@gmail.com>
2025-07-15 18:23:10 +0300

    feat: add partnership video using iframe and instagram

--- src/components/sections/PartnerImpact.tsx
+++ src/components/sections/PartnerImpact.tsx
@@ -8,8 +8,27 @@ const PartnerImpact = ({data}: PartnerImpactProps) => {
     return (
         <>
             <div className='flex flex-col px-8 space-y-6 pt-9 lg:pt-[74px] lg:px-[82px] lg:space-y-12'>
-                <h1 className='text-5xl font-black text-[#1B3959]'>{data.impact.title}</h1>
-                <p className='lg:w-[675px] text-lg font-medium'>{data.impact.description}</p>
+                <div className="flex flex-col lg:flex-row items-stretch justify-center gap-8 mt-8 w-full">
+                    {/* Text Content */}
+                    <div className="flex-1 flex flex-col justify-center bg-white p-6 rounded-xl shadow-sm min-h-[400px]">
+                        <h1 className='text-5xl font-black text-[#1B3959]'>{data.impact.title}</h1>
+                        <p className='text-lg font-medium mt-4'>{data.impact.description}</p>
+                    </div>
+                    {/* Video Section */}
+                    <div className="flex-1 flex items-center justify-center min-h-[400px]">
+                        <iframe
+                            src="https://www.instagram.com/reel/DL47_CoI7vr/embed"
+                            width="100%"
+                            height="400"
+                            allow="autoplay; encrypted-media"
+                            allowFullScreen
+                            title="Instagram Reel"
+                            className="rounded-xl border-none w-full h-full"
+                        ></iframe>
+                    </div>
+                </div>
+
+                
                 <div className='custom-gradient flex flex-col lg:flex-row justify-between gap-8 p-6 md:p-16 rounded-2xl text-white'>
                     {data.impact.data.map((item, i) => (
                         <div className='space-y-3 text-center' key={i}>
@@ -18,7 +37,12 @@ const PartnerImpact = ({data}: PartnerImpactProps) => {
                         </div>
                     ))}
                 </div>
+
+
+
             </div>
+
+
         </>
     );
 };
